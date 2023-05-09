6be167d3499e6cf2268dcd8c56dc471bd5cc422a
J0onYEong <cjy2134@naver.com>
2023-05-09 15:17:58 +0900

    [Refactor] Boj/1277/코드수정

--- cpp/1277/solution.cpp
+++ cpp/1277/solution.cpp
@@ -3,11 +3,21 @@
 #define endl "\n"
 #define vertex pair<int, int>
 #define edge pair<int, int>
-#define MAX_INT 2147483647
+#define MAX 2000000000
 
 using namespace std;
 
-double get_length(vertex v1, vertex v2) {
+vector<edge> edges;
+vector<vertex> vertices;
+
+double get_length(int n1, int n2) {
+    for(auto e : edges) {
+        if((e.first==n1 && e.second==n2) || (e.first==n2 && e.second == n1))
+            return 0;
+    }
+    vertex v1 = vertices[n1];
+    vertex v2 = vertices[n2];
+
     double a = pow((v1.first-v2.first), 2);
     double b = pow((v1.second-v2.second), 2);
     return sqrt(a+b);
@@ -17,53 +27,47 @@ void solution() {
     int n, w;
     double m;
     cin >> n >> w >> m;
-    vector<vertex> vertices;
     vertices.push_back(make_pair(0,0));
     for(int i=1; i<=n; i++) {
         vertex temp;
         cin >> temp.first >> temp.second;
         vertices.push_back(temp);
     }
-    
-    vector<edge> edges;
     for(int i=0; i<w; i++) {
         edge temp;
         cin >> temp.first >> temp.second;
         edges.push_back(temp);
-    }    
+    }
 
-    priority_queue<pair<int, int> > pq;
+    priority_queue<pair<double, int> > pq;
     pq.push(make_pair(0, 1));
-    int d[1001];
-    memset(d, MAX_INT, sizeof(d));
+    double d[1001];
+    bool visit[1001];
+    for(int i=0; i<1001; i++) {
+        d[i] = MAX;
+        visit[i] = false;
+    }
 
-    while(true) {
+    while(!pq.empty()) {
         int v = pq.top().second;
-        cout << v << endl;
-        int weight = pq.top().first;
-
-        if(v == n) 
+        visit[v] =true;
+        double weight = -pq.top().first;
+        pq.pop();
+        if(v == n)
             break;
-
-        for(auto e : edges) {
-            if(e.first == v) {
-                pq.push(make_pair(weight, e.second));
-            } else if(e.second == v) {
-                pq.push(make_pair(weight, e.first));
-            }
-        }
-
-        for(int i=1; i<=v; i++) {
-            int temp_w = get_length(vertices[v], vertices[i]);
+        for(int i=1; i<=n; i++) {
+            if(visit[i])
+                continue;
+            double  temp_w = get_length(v, i);
             if(temp_w >= m)
                 continue;
             if(d[i] > temp_w+weight) {
                 d[i] = temp_w + weight;
-                pq.push(make_pair(temp_w + weight, i));
+                pq.push(make_pair(-d[i], i));
             }
         }
     }
-    cout << d[n] << endl;
+    cout << (int)(d[n]*1000) << endl;
 }
 
 int main() {
