ed6af5715897ad2a9fe825ddc02eafc33a17820b
J0onYEong <cjy2134@naver.com>
2023-05-31 16:09:46 +0900

    [Fix] Boj/18513/Gold4/BFS/Data-structure Fix : 18513
    완전탐색으로 시도한 첫시도는 시간초과 오류를 피할 수 없었다.
    이문제는 BFS를 활용하여 해결이 가능했다.
    - 더이상 탐색이 필요없는 좌표를 거를 수 있었다.
    - 매 반복마다 정해진 깊이가 있음으로 탐색마다 삽입위치의 깊이(문제에서의 불행도)를 측정할 필요가 없다.

--- cpp/18513/solution.cpp
+++ cpp/18513/solution.cpp
@@ -4,59 +4,45 @@
 
 using namespace std;
 
-int getPosFromLevel(int mid, int lev) {
-    int dis = (lev/2) + (lev%2);
-    bool isOdd = lev % 2;
-    return mid + dis * (isOdd ? 1 : -1);
-}
-
 void solution() {
     int N, K;
-    vector<int> pool_pos;
-
-    //0 - left_bound, 1 - right_bound
-    int pool_data[100000][2];
-
     cin >> N >> K;
-    for(int i = 0; i<N; i++) {
-        int pos;
-        cin >> pos;
-        pool_pos.push_back(pos);
-    }
-    sort(pool_pos.begin(), pool_pos.end());
-    for(int i=0; i<N; i++) {
-        pool_data[i][0] = pool_pos[i];
-        pool_data[i][1] = pool_pos[i];
-    }
-    
-    ll result = 0;
-    for(int level = 1; K > 0; level++) {
-        for(int i=0; i < N; i++) {
-            int middlePos = pool_pos[i];
-            int now_pos = getPosFromLevel(middlePos, level);
 
-            //오른쪽 이동
-            if(level % 2 != 0) {
-                //오른쪽 다음 노드의 왼쪽 바운더리가 현재 삽입하려는 위치보다 작거나 같으면 삽입불가
-                if(i < N-1)
-                    if(pool_data[i+1][0] <= now_pos)
-                        continue;
-                pool_data[i][1] = now_pos;
-            } else {
-                //왼쪽 이동
-                if(i > 0) {
-                    if(pool_data[i-1][1] >= now_pos)
-                        continue;
-                }
-                pool_data[i][0] = now_pos;
-            }
-            result += abs(now_pos - middlePos);
-            K--;
-            if(K == 0)
-                break;
-        }
+    queue<int> search;
+    set<int> visit;
+    for(int i=0; i<N; i++) {
+        int temp;
+        cin >> temp;
+        search.push(temp);
+        visit.insert(temp);
     }
-    cout << result << endl;
+
+    int horSearch[] = {-1, 1};
+    ll result = 0;
+    int depth = 1;
+    while(!search.empty()) {
+        int size = search.size();
+
+        while(size--) {
+            int temp = search.front();
+            search.pop();
+
+            for(int i=0; i<2; i++) {
+                int ins = temp + horSearch[i];
+                if(visit.count(ins) < 1) {
+                    visit.insert(ins);
+                    search.push(ins);
+                    result += depth;
+                    K--;
+                    if(K == 0) {
+                        cout << result << endl;
+                        return;
+                    }
+                }
+            }
+        }
+        depth++;
+    }
 }
 
 int main() {
