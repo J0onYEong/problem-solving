c9cd35c8aa6221dfa5b386fae740d9dc8c62c227
J0onYEong <cjy2134@naver.com>
2023-06-28 22:05:20 +0900

    [Add] Boj/9372

---
+++ cpp/9372/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+
+using namespace std;
+
+void solution() {
+    int T;
+    cin >> T;
+
+    for(int t=0; t<T; t++) {
+        int N, M;
+        cin >> N >> M;
+        for(int i=0; i<M; i++) {
+            int a, b;
+            cin >> a >> b;
+        }   
+        cout << N-1 << endl;
+    }
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    solution();
+    return 0;
+}
