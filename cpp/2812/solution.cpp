aa7e520f0cb51230f4d64a91c1243a7d6b31412b
J0onYEong <cjy2134@naver.com>
2023-07-03 17:50:23 +0900

    [Fix] Boj/2812/Gold3/Data-Structure/deque Fix : 2812
    Deque를 사용하여 풀이한 문제이다. Deque는 vector처럼 인덱스로 접근이 가능하면 서도 queue와 stack의 특성을 모두 가지고 있다.

--- cpp/2812/solution.cpp
+++ cpp/2812/solution.cpp
@@ -1,43 +1,31 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define endl "\n"
+#define con pair<int, int>
 
 using namespace std;
-const int MAX = 500000;
-
-int N, K;
-string number_str;
-
-void find_number(int start_index, int rest_k) {
-    if(N - start_index == rest_k) {
-        cout << endl;
-        return;
-    }
-
-    int end_index = start_index + rest_k;
-
-    char max_number = '0';
-    for(int i=start_index; i<=end_index; i++)
-        max_number = max(max_number, number_str[i]);
-
-    cout << max_number;
-    int temp_start = start_index;
-    for(int i=start_index; i<=end_index; i++) {
-        if(number_str[i] == max_number)
-            break;
-        temp_start++;
-        rest_k--;
-    }
-
-    find_number(temp_start+1, rest_k)
-}
 
 void solution() {
-    
-    cin >> N >> K >> number_str;
+    int N, K;
+    string num_str;
+    cin >> N >> K >> num_str;
 
+    deque<int> dq;
 
-    find_number(0, K);
+    int k = K;
+
+    for(int i=0; i<N; i++) {
+        int now = num_str[i]-48;
+        while(!dq.empty() && k > 0 && dq.back() < now) {
+            dq.pop_back();
+            k--;
+        }
+        dq.push_back(now);
+    }
+
+    for(int i=0; i<N-K; i++)
+        cout << dq[i];
+    cout << endl;
 }
 
 int main() {
