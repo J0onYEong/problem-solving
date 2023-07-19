7a9bc7563f629848f6667e84646adc9112be3d09
J0onYEong <cjy2134@naver.com>
2023-07-19 15:01:11 +0900

    [Feat] Boj/1343/using replace & find method

--- python/1343/solution.py
+++ python/1343/solution.py
@@ -1,37 +1,15 @@
 
 
 inputStr = input()
-inputStr+=' '
-
-p1 = 'AAAA'
-p2 = 'BB'
+p1='AAAA'
+p2='BB'
 resultStr = ""
 
-check = False
-xCount = 0
-for i in range(len(inputStr)):
-    if inputStr[i] == 'X':
-        if check:
-            xCount+=1
-            if xCount % 4 == 0:
-                resultStr+=p1
-                xCount=0
-                check = False
-        else:
-            xCount=1
-            check=True
-    else:
-        if check:
-            if xCount % 2 == 0:
-                resultStr+=p2
-                check=False
-                xCount=0
-            else:
-                print(-1)
-                exit()
-        if inputStr[i] == '.':
-            resultStr+='.'
+inputStr=inputStr.replace('XXXX', 'AAAA')
+inputStr=inputStr.replace('XX', 'BB')
 
-print(resultStr)
-        
-        
+if inputStr.find('X') == -1:
+    print(inputStr)
+else:
+    print(-1)
+
