bdb93fac684d16fad2d889cfeb8f21a85afbdff8
J0onYEong <cjy2134@naver.com>
2023-08-10 15:20:47 +0900

    [Add] Boj/1026/Silver4/Sort

---
+++ python/1026/solution.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+result=0
+
+for index in range(N):
+    result += A[index] * B[index]
+
+print(result)
