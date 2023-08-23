c10542c1711e60e0d4fc6287fd181156c2d8928a
J0onYEong <cjy2134@naver.com>
2023-08-23 15:42:31 +0900

    [Fix] Boj/11727/Error fix

--- python/11727/solution.py
+++ python/11727/solution.py
@@ -4,15 +4,12 @@ ipt = sys.stdin.readline
 
 n = int(ipt())
 
-dp=[0 for _ in range(n+1)]
-dp[0] = 0
-dp[1] = 1
-dp[2] = 3
+dp = [0, 1, 3]
 
 mod = 10007
 
 # 3 ~ n
 for i in range(3, n+1):
-    dp[i] = dp[i-1] + 2 * dp[i-2]
+    dp.append(dp[i-1] + 2 * dp[i-2]) 
 
 print(dp[n] % mod) 
