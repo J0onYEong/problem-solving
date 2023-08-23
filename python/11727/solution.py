cd2db41817a5ea02cb85cf1ddfd5139c4f2bddb1
J0onYEong <cjy2134@naver.com>
2023-08-23 15:32:18 +0900

    [Add] Boj/11727/Dp/효율적 풀이

--- python/11727/solution.py
+++ python/11727/solution.py
@@ -4,24 +4,15 @@ ipt = sys.stdin.readline
 
 n = int(ipt())
 
-dp=[-1 for _ in range(n+1)] 
+dp=[0 for _ in range(n+1)]
+dp[0] = 0
+dp[1] = 1
+dp[2] = 3
 
 mod = 10007
 
-def recursive(pos):
-    global n
+# 3 ~ n
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + 2 * dp[i-2]
 
-    if pos == n+1:
-        return 1;
-
-    if dp[pos] != -1:
-        return dp[pos];
-
-    dp[pos] = 0
-        
-    dp[pos] += recursive(pos+1) % mod
-    if pos+2 <= n+1:
-        dp[pos] += 2*(recursive(pos+2) % mod)
-    return dp[pos] % mod
-
-print(recursive(1))
+print(dp[n] % mod) 
